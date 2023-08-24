9ff37c3ed419680158308a645e7e1b9497247654
Pamela Lima <batistalimapamela69@gmail.com>
2023-08-24 10:58:23 -0300

    finalizando pagina pedidos

--- react-sprint2/src/components/FormServices/index.tsx
+++ react-sprint2/src/components/FormServices/index.tsx
@@ -1,6 +1,6 @@
 import Button from "../Botao";
 import { Form, Container } from "./style";
-import { FormEventHandler, useState } from "react";
+//import { FormEventHandler, useState } from "react";
 
 export default function FormService() {
     return (
@@ -10,40 +10,41 @@ export default function FormService() {
                     <label>Nome do Cliente</label>
                     <input
                         type="text"
-                        value="Maria"
+                        value="  Maria da Silva"
                         disabled
                     />
 
                     <label>Serviço Solicitado</label>
                     <input
                         type="text"
-                        value="Plano 1 - Instalação"
+                        value="  Plano 1 - Instalação"
                         disabled
                     />
 
                     <label>Data da contratação</label>
                     <input
-                        type="date"
-                        value="26/06/2023"
+                        type="text"
+                        value="  26/06/2023"
                         disabled
                     />
 
                     <label>Horário sugerido</label>
                     <input
                         type="hour"
-                        value="08:00 - 12:00"
+                        value="  08:00 - 12:00"
                         disabled
                     />
 
                     <label>Status</label>
                     <input
                         type="status"
-                        value="Aguardando agendamento"
+                        value="  Aguardando agendamento"
                         disabled
                     />
-                </Form>
-                <Button text="voltar" />
+                     <Button text="voltar" />
                 <Button text="atender solicitação" />
+                </Form>
+            
             </Container>
         </>
     );
--- react-sprint2/src/components/FormServices/style.ts
+++ react-sprint2/src/components/FormServices/style.ts
@@ -2,14 +2,33 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   width: 450px;
+  height: 500px;
   background-color: #fff;
   padding: 30px 35px;
   margin: 50px 0;
-  border: 1px solid blue;
-  border-radius: 15px;
+  padding: 40px;
+  border-radius: 10px;
+  background-color: #fff;
+  border: 1px solid rgb(210, 210, 210);
+  box-shadow: 10px 5px 5px rgb(210, 210, 210);
+  font-size: 17px;
+  font-weight: 600;
+  
+  
+  
+  
 
   input {
     width: 100%;
+    height: 40px;
+    border-radius: 10px;
+    font-size: 15px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+    font-weight: 500;
+    background-color: #f4f4ec;
+    border: 0px;
+  
   }
 `;
 
@@ -17,4 +36,5 @@ export const Container = styled.div`
   width: 1200px;
   max-width: 100%;
   margin: 0 auto;
+
 `;
