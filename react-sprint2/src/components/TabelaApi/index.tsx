fcf3fb99283f930a48924711b71640e2b41d6d48
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-09-28 22:46:29 -0300

    arrumando bug id tabela

--- react-sprint2/src/components/TabelaApi/index.tsx
+++ react-sprint2/src/components/TabelaApi/index.tsx
@@ -1,9 +1,19 @@
 import { useState, useEffect } from "react";
 import Botao from '../Botao';
-import Modal from '../Modal';
 import { ContainerTabela, Info, PosicaoBotao } from './style';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { BotaoLogin, BotaoPainelVerde } from '../Botao/style';
+import { Estilo } from './style';
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 interface Order {
     id: number;
@@ -16,18 +26,25 @@ interface Order {
     status:string;
 }
 
+let id: number;
+
+
+
 export default function TabelaApi() {
+
     const [order, setOrder] = useState <Order[]>([]);
 
-    useEffect(() => {
+   
+    useEffect(()=>{
         axios.get('http://localhost:3000/order')
-            .then(response => {
-                setOrder(response.data);
-            })
-            .catch(error => {
-                console.error('Erro ao buscar os pedidos:', error);
-            });
-    }, []);
+        .then(response => {
+            setOrder(response.data);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar os pedidos:', error);
+        });
+   }, []);
+  
 
     return (
         <>
@@ -53,10 +70,10 @@ export default function TabelaApi() {
                                     <td align="right">{order.status}</td>
                                     <PosicaoBotao>
                                         <td>
-                                            <Link to={`/detalhe/${order.id}`}>
+                                            <Link to={`/detalhe/${order.id, id = order.id}`}>
                                                 <Botao type='botaoPainelAzul' text='ver detalhes' />
                                             </Link>
-                                            <Modal />
+                                            <AlertDialogSlide />
                                         </td>
                                     </PosicaoBotao>
                                 </tr>
@@ -67,4 +84,90 @@ export default function TabelaApi() {
             </ContainerTabela>
         </>
     );
+}
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+  function AlertDialogSlide() {
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const [status, setStatus] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+      setStatus(event.target.value);
+  };
+
+  const handleStatusUpdate  = async () =>{
+       const response = await axios.put(`http://localhost:3000/order/${id}`,{status: status});
+        if(response.status == 200){console.log("Status atualizado com sucesso!" + id);} 
+        else{console.log("Erro ao atualizar o status do pedido.")}
+    }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [order, setOrder] = useState <Order[]>([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/order')
+    .then(response => {
+        setOrder(response.data);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar os pedidos:', error);
+    });
+}, []);
+
+  return (
+    <div>
+        
+      <BotaoPainelVerde onClick={handleClickOpen}>atender solicitação</BotaoPainelVerde>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        onClick={(event) => { event.defaultPrevented }}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <Estilo><h1> Atualizar Status </h1>
+          
+        </Estilo>
+        <FormControl sx={{ m: 1, minWidth: 300 }} size="small">
+            <InputLabel id="demo-select-small-label">Status</InputLabel>
+            <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={status}
+                label="status"
+                onChange={handleChange}
+            >
+                <MenuItem value="">
+                </MenuItem>
+                <MenuItem value={"CANCELADO"}>CANCELADO</MenuItem>
+                <MenuItem value={"ACEITO"}>ACEITO</MenuItem>
+                <MenuItem value={"EM ANDAMENTO"}>EM ANDAMENTO</MenuItem>
+                <MenuItem value={"FINALIZADO"}>FINALIZADO</MenuItem>
+                <MenuItem value={"ATRASADO"}>ATRASADO</MenuItem>
+            </Select>
+        </FormControl>
+        <Button onClick={handleStatusUpdate}>Atualizar</Button>
+        <Button onClick={handleClose}>Voltar</Button>
+      </Dialog>
+    </div >
+  );
 }
--- react-sprint2/src/components/TabelaApi/style.ts
+++ react-sprint2/src/components/TabelaApi/style.ts
@@ -50,6 +50,29 @@ export const PosicaoBotao = styled.div`
 `;
 
 
+export const Estilo = styled.div`
+    margin: 50px;
+    padding-left: 10px;
+    
+    h1{
+        margin-top:0px;
+        padding-left: 90px;
+        padding-right: 100px;
+        text-transform:uppercase;
+        color: #556FF5;
+        font-size:20px;
+    
+    }
+
+    button {
+        color: red;
+        font-size: 200px;
+    }
+    
+`;
+
+
+
 
 
 
