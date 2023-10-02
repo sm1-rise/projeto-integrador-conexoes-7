79ddb9cea152ea64396e80fede9cfd0857588482
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-10-01 22:22:34 -0300

    Adicionando GET Pedido para pagaina de solicitações do cliente

--- index.js
+++ index.js
@@ -1,51 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var contratarButton = document.getElementById("contratar-button");
+    var codigoProdutoInput = document.getElementById("codigo");
+    var resultadoStatus = document.getElementById("resultado-apiStatus");
+    var resultadoNome= document.getElementById("resultado-apiNome");
+    var resultadoPlano = document.getElementById("resultado-apiPlano");
 
-const url ="http://localhost:3000/order";
+    contratarButton.addEventListener("click", function (event) {
+        event.preventDefault();
 
-function getOrder(event) {
-   event.preventDefault();
-    const codigoPedido =  document.getElementById("codigo").value;
-    
-    axios.get(`${url}/${codigoPedido}`)
-    .then(response =>{
-        const data = response.data
-        console.log(data);
-        renderResults = document.getElementById("renderResults");
-        renderResults.textContent = JSON.stringify(data);
-    })
-    .catch(error =>{console.log(error)
+        var codigoProduto = codigoProdutoInput.value;
+        var apiUrl = "http://localhost:3000/order/" + codigoProduto;
+
+        axios.get(apiUrl)
+            .then(function (response) {
+                const data = response.data
+                console.log("Resposta da API:", data);
+
+                var nome = response.data.nome;
+                var plano = response.data.servico_escolhido;
+                var status = response.data.status;
+                
+                resultadoStatus.textContent = 'Status da solicitação: ' + status;
+                resultadoNome.textContent = 'Nome do solicitante: ' + nome;
+                resultadoPlano.textContent = 'Plano escolhido : ' + plano;
+                ;
+               
+            })
+            .catch(function (error) {
+                console.error("Erro na solicitação:", error);
+                if (error.response && error.response.status === 404) {
+                    resultadoNome.textContent = "";
+                    resultadoPlano.textContent = "";
+                    resultadoStatus.textContent = "Não foi possível encontrar seu pedido, por favor verifique o código do pedido.";
+                }
+            });
     });
-}
+});
 
-getOrder();
-
-
-const newOrder = {
-        nome: "João Marcelo",
-        cpf: "123.456.789-08",
-        telefone: "(11) 98765-4371",
-        servico_escolhido: "Instalação de Fibra Óptica",
-        preferencia_horario: "12h às 15h",
-        status: "ACEITO"
-}
-
-function  addNewOrder(){
-    axios.post(url, newOrder)
-    .then(response => {
-        alert()
-    })
-    .catch(error => console.log (error))
-}
-
-// addNewOrder();
-
-
-function getOneUser(){
-    axios.get(`${url}/3`)
-    .then(response =>{
-        const data = response.data
-        renderResults.textContent = JSON.stringify(data);
-    })
-    .catch(error =>{console.log(error)
-    });
-}
-// getOneUser();
